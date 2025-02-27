e7f82b6c80a4a7731c756eb430ac6cf270f37d3b
ayus-aryal <ayusaryal411@gmail.com>
2025-02-27 22:41:54 +0530

    Crafted the Home Screen for issue #2 and checked for any existing conflicts.

--- composeApp/src/commonMain/kotlin/est/mkao/emergency/response/App.kt
+++ composeApp/src/commonMain/kotlin/est/mkao/emergency/response/App.kt
@@ -1,20 +1,6 @@
 package est.mkao.emergency.response
-
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import est.mkao.emergency.response.screens.HomeScreen
-import org.jetbrains.compose.resources.painterResource
-
-import nurseresponder.composeapp.generated.resources.Res
-import nurseresponder.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun App() {
--- composeApp/src/commonMain/kotlin/est/mkao/emergency/response/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/est/mkao/emergency/response/screens/HomeScreen.kt
@@ -12,11 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.foundation.clickable
-
-
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-
 import androidx.compose.ui.unit.dp
 
 @Composable
